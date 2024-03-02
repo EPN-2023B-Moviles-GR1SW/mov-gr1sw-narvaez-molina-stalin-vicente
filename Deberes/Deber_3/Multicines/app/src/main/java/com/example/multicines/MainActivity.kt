d0252c88d78804bfeaec22fcf673247605b97849
Vicente0709 <stalin.narvaez02@epn.edu.ec>
2024-03-02 18:05:46 -0500

    navegación entre las vistas
    navegación y configuración de iconos y colores de replicar lo mas posible a la app de multicines

--- Deberes/Deber_3/Multicines/app/src/main/java/com/example/multicines/MainActivity.kt
+++ Deberes/Deber_3/Multicines/app/src/main/java/com/example/multicines/MainActivity.kt
@@ -4,13 +4,55 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.btnNavInicio -> {
+                    replaceFragment(inicioFragmentView())
+                    true
+                }
+
+                R.id.btnNavCartelera -> {
+                    replaceFragment(carteleraFragmentView())
+                    true
+                }
+
+                R.id.btnNavDulceria -> {
+                    replaceFragment(dulceriaFragmentView())
+                    true
+                }
+
+                R.id.btnNavMulticlub -> {
+                    replaceFragment(multiclubFragmentView())
+                    true
+                }
+
+                R.id.btnNavPerfil -> {
+                    replaceFragment(perfilFragmentView())
+                    true
+                }
+
+                else -> false
+            }
+        }
+        replaceFragment(inicioFragmentView())
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frameContainer, fragment).commit()
+
+    }
+
     //mostrar mensajes
     fun mostrarSnackbar(texto: String) {
         Snackbar.make(
