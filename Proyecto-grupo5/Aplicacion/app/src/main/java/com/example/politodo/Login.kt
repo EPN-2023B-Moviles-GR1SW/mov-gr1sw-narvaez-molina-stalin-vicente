5e7b8a44251a7f89d53b821680489a6cb8cb8971
Vicente0709 <stalin.narvaez02@epn.edu.ec>
2024-03-03 18:58:16 -0500

    update views and auth firebase
    Bottom navigation, Firebase Auth, and updated views

--- Proyecto-grupo5/Aplicacion/app/src/main/java/com/example/politodo/Login.kt
+++ Proyecto-grupo5/Aplicacion/app/src/main/java/com/example/politodo/Login.kt
@@ -1,13 +1,81 @@
 package com.example.politodo
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
+    private lateinit var txtUserLogin: EditText
+    private lateinit var txtPasswordUser: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Inicializar Firebase Authentication
+        auth = FirebaseAuth.getInstance()
 
+        // Referenciar los EditText y el Button desde el layout
+        txtUserLogin = findViewById(R.id.txtUserLogin)
+        txtPasswordUser = findViewById(R.id.txtPasswordUser)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        //listener del boton
+        btnLogin.setOnClickListener{
+            val email = txtUserLogin.text.toString()
+            val password = txtPasswordUser.text.toString()
+
+            println("USer: ${email} \nand \nPassword: ${password}")
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                println(" Entro al IF : USer: ${email} \nand \nPassword: ${password}")
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Inicio de sesión exitoso, actualizar la interfaz de usuario con la información del usuario registrado
+                            val user = auth.currentUser
+                            print(user)
+                            // Aquí puedes llamar a una función para actualizar la interfaz de usuario (UI) con la información del usuario
+                            irActividad(MainActivity::class.java)
+                        } else {
+                            // Ocurrió un error durante el inicio de sesión, mostrar un mensaje de error al usuario
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // Aquí puedes llamar a una función para actualizar la interfaz de usuario (UI) en caso de falla de inicio de sesión
+                            println("no  se pudo loguear ")
+                        }
+                    }
+            } else {
+                // Mostrar un mensaje de error si el correo electrónico o la contraseña están vacíos
+                Toast.makeText(
+                    this, "Please enter email and password.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }
+
+    }
+
+
+    fun irActividad(
+        clase: Class<*>
+
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
--- Proyecto-grupo5/Aplicacion/app/src/main/java/com/example/politodo/MainActivity.kt
+++ Proyecto-grupo5/Aplicacion/app/src/main/java/com/example/politodo/MainActivity.kt
@@ -7,14 +7,76 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var auth: FirebaseAuth
+    private lateinit var btnAddCategory: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_nav_search -> {
+
+                  true
+                }
+
+                R.id.menu_nav_home-> {
+                    Toast.makeText(
+                        baseContext,
+                        "Home",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    true
+                }
+
+                R.id.menu_nav_settings-> {
+                    irActividad(Settings::class.java)
+                    true
+                }
+                else -> false
+            }
+        }
+
+        btnAddCategory
+            .setOnClickListener{
+                irActividad(CrearCategoria::class.java)
+            }
+
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            irActividad(Login::class.java)
+        }
+    }
+    fun abrirLogin() {
+        irActividad(Login::class.java)
+    }
+
+    fun irActividad(
+        clase: Class<*>
+
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
