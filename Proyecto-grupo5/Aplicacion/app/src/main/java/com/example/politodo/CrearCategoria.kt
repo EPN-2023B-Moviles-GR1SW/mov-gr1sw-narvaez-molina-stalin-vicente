0fc62f1b6512246f70ae04fb010114f954df5344
Vicente0709 <stalin.narvaez02@epn.edu.ec>
2024-03-03 09:39:35 -0500

    implement views add task and add category
    views

---
+++ Proyecto-grupo5/Aplicacion/app/src/main/java/com/example/politodo/CrearCategoria.kt
@@ -0,0 +1,11 @@
+package com.example.politodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearCategoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_categoria)
+    }
+}
---
+++ Proyecto-grupo5/Aplicacion/app/src/main/java/com/example/politodo/CrearTarea.kt
@@ -0,0 +1,11 @@
+package com.example.politodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearTarea : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_tarea)
+    }
+}
